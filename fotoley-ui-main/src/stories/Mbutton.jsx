ee2cfef6fc8693f03f425e96f7f1110c1268c1b7
Himanshu Bhatti <himanshubhatti@uiit.ac.in>
2021-11-02 12:52:29 +0530

    initial cmt

---
+++ fotoley-ui-main/src/stories/Mbutton.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Mbutton = ({label}) => {
+    return (
+        <button>{label}</button>
+    );
+}
+
+export default Mbutton;
---
+++ fotoley-ui-main/src/stories/Mbutton.stories.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import Mbutton from './Mbutton'
+export default{
+    title: "Example/Mbutton",
+    component: Mbutton
+}
+export const Primary=()=><Mbutton label="hi"/>
